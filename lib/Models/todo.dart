15cb44bbd9186e3a7bbed147061298f668771d7d
ashwabu <abdulazizredi90@gmail.com>
2023-12-14 08:38:40 +0300

    Todo cretion

---
+++ lib/Models/todo.dart
@@ -0,0 +1,10 @@
+
+class Todo{
+  int id;
+  String title;
+  String description;
+  bool isDone;
+
+  Todo({required this.id,required this.title,required this.description,required this.isDone});
+
+}
