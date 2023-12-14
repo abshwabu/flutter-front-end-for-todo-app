1193a27ba7697705a138345def1bed286350a3c0
ashwabu <abdulazizredi90@gmail.com>
2023-12-14 08:15:18 +0300

    intial

---
+++ lib/pages/homepage.dart
@@ -0,0 +1,19 @@
+// ignore_for_file: unused_import
+
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      
+    );
+  }
+}
