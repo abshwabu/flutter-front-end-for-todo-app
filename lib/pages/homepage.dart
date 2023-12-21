a564829b7a91ad87fd6c61a69a0e29747a1fd543
ashwabu <abdulazizredi90@gmail.com>
2023-12-21 12:04:44 +0300

    text field update

--- lib/pages/homepage.dart
+++ lib/pages/homepage.dart
@@ -122,7 +122,20 @@ class _HomePageState extends State<HomePage> {
                           ),
                         ),
                         Gap(15),
-                      TextField()
+                        TextFormField(
+                          decoration: const InputDecoration(
+                            icon: Icon(Icons.person),
+                            hintText: 'What do people call you?',
+                            labelText: 'Name *',
+                          ),
+                          onSaved: (String? value) {
+                            // This optional block of code can be used to run
+                            // code when the user saves the form.
+                          },
+                          validator: (String? value) {
+                            return (value != null && value.contains('@')) ? 'Do not use the @ char.' : null;
+                          },
+                        ),
                       ],
                     )
                 ),
