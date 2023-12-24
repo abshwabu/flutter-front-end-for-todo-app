37a37c9a9d1676bfba3c76935bd2c18038bc08c3
ashwabu <abdulazizredi90@gmail.com>
2023-12-24 11:05:41 +0300

    updating task started

--- lib/widget/todo_container.dart
+++ lib/widget/todo_container.dart
@@ -13,39 +13,44 @@ class TodoContainer extends StatelessWidget {
   Widget build(BuildContext context) {
     return Padding(
       padding:const EdgeInsets.all(15),
-    child: Container(
-      width: double.infinity,
-      height: 120,
-      decoration: BoxDecoration(
-        color: isDone ? green: red,
-        borderRadius: BorderRadius.all(Radius.circular(4),
-        
-        )
-      ),
-      child: Padding(
-        padding: const EdgeInsets.all(18.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children:[ Text(title, style:const TextStyle(
+    child: InkWell(
+      onTap: (){
+        print('tapped');
+      },
+      child: Container(
+        width: double.infinity,
+        height: 120,
+        decoration: BoxDecoration(
+          color: isDone ? green: red,
+          borderRadius: BorderRadius.all(Radius.circular(4),
+
+          )
+        ),
+        child: Padding(
+          padding: const EdgeInsets.all(18.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children:[ Text(title, style:const TextStyle(
+                  color: Colors.white,
+                  fontSize: 18,
+                  fontWeight: FontWeight.bold
+                ),
+                ),
+                IconButton(onPressed:() => onPress(), icon: const Icon(Icons.delete),)
+                ]
+              ),
+              Text(description, style:const TextStyle(
                 color: Colors.white,
-                fontSize: 18,
-                fontWeight: FontWeight.bold
+                fontWeight: FontWeight.normal,
+                fontSize: 15
+              )
               ),
-              ),
-              IconButton(onPressed:() => onPress(), icon: const Icon(Icons.delete),)
-              ]
-            ),
-            Text(description, style:const TextStyle(
-              color: Colors.white,
-              fontWeight: FontWeight.normal,
-              fontSize: 15
-            )
-            ), 
-            // 
-          ],
+              //
+            ],
+          ),
         ),
       ),
     )
