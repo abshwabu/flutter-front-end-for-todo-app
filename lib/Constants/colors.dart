3217ce709ec8a59b81d5965d67b97caae2d0f6f4
ashwabu <abdulazizredi90@gmail.com>
2023-12-24 10:44:41 +0300

    background colors added

--- lib/Constants/colors.dart
+++ lib/Constants/colors.dart
@@ -4,3 +4,4 @@ import 'package:flutter/material.dart';
 const Color green = Color(0xff08bd08);
 const Color red = Color(0xffeb5757);
 const Color darkBlue = Color(0xff2d81f7);
+const Color background = Color(0xfffafbfc);
